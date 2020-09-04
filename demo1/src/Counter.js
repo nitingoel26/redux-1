2baaacf1a4ab8f9b848c533dc2caa67a0457b5e3
Satuti Garg <satuti@competentgroove.com>
2020-09-04 18:12:48 +0530

    using context consumer

--- demo1/src/Counter.js
+++ demo1/src/Counter.js
@@ -1,21 +1,26 @@
 import React, { Component } from "react";
 import { CounterContext } from "./CounterContext";
 export default class Counter extends Component {
-  incValue = this.incValue.bind(this);
   render() {
-    let { counter, incValue, decValue } = this.context;
-
     return (
-      <div>
-        <label>{counter}</label>
-        <br />
-        <button className="increment" onClick={incValue}>
-          Increment
-        </button>
-        <button className="decrement" onClick={decValue}>
-          Decrement
-        </button>
-      </div>
+      <CounterContext.Consumer>
+        {(context) => {
+          let { counter, incValue, decValue } = context;
+          console.log(context);
+          return (
+            <div>
+              <label>{counter}</label>
+              <br />
+              <button className="increment" onClick={incValue}>
+                Increment
+              </button>
+              <button className="decrement" onClick={decValue}>
+                Decrement
+              </button>
+            </div>
+          );
+        }}
+      </CounterContext.Consumer>
     );
   }
 }
