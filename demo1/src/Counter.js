5c20b24d27e9c44190800c6f40badeb2c4d3234e
Satuti Garg <satuti@competentgroove.com>
2020-09-04 18:36:26 +0530

    using useContext

--- demo1/src/Counter.js
+++ demo1/src/Counter.js
@@ -1,26 +1,20 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { CounterContext } from "./CounterContext";
-export default class Counter extends Component {
-  render() {
-    return (
-      <CounterContext.Consumer>
-        {(context) => {
-          let { counter, incValue, decValue } = context;
-          console.log(context);
-          return (
-            <div>
-              <label>{counter}</label>
-              <br />
-              <button className="increment" onClick={incValue}>
-                Increment
-              </button>
-              <button className="decrement" onClick={decValue}>
-                Decrement
-              </button>
-            </div>
-          );
-        }}
-      </CounterContext.Consumer>
-    );
-  }
+export default function Counter() {
+  const context = useContext(CounterContext);
+
+  let { counter, incValue, decValue } = context;
+  console.log(context);
+  return (
+    <div>
+      <label>{counter}</label>
+      <br />
+      <button className="increment" onClick={incValue}>
+        Increment
+      </button>
+      <button className="decrement" onClick={decValue}>
+        Decrement
+      </button>
+    </div>
+  );
 }
